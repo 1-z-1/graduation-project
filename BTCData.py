17d33e59e255a5b2e028882cadf3957103c328e1
Lucifer <2423808173@qq.com>
2022-03-12 22:25:10 +0800

    complished the Bp Neutron Network

--- BTCData.py
+++ BTCData.py
@@ -6,7 +6,6 @@ import pandas as pd
 import talib
 import os
 
-
 os.chdir('C:/Users/24238/Desktop/毕业设计/毕业论文代码')
 
 # 请求数据
@@ -32,19 +31,20 @@ with urllib.request.urlopen(
         valueMin.append(priceDict[i].split(',')[2])
         value.append(priceDict[i].split(',')[4])
         deal.append(priceDict[i].split(',')[5])
-data = pd.DataFrame({'date':date,'OPEN':valueStart,'MAX':valueMax,'MIN':valueMin,'CLOSE':value,'VOLUME':deal})
+data = pd.DataFrame(
+    {'date': date, 'OPEN': valueStart, 'MAX': valueMax, 'MIN': valueMin, 'CLOSE': value, 'VOLUME': deal})
 # 计算拓展指标
 data['CCI'] = talib.CCI(data['MAX'], data['MIN'], data['CLOSE'], timeperiod=15)
-data['SLOWK'],data['SLOWD'] = talib.STOCH(data['MAX'],data['MIN'],data['CLOSE'])
-data['FASTK'],data['FASTD'] = talib.STOCHF(data['MAX'],data['MIN'],data['CLOSE'])
+data['SLOWK'], data['SLOWD'] = talib.STOCH(data['MAX'], data['MIN'], data['CLOSE'])
+data['FASTK'], data['FASTD'] = talib.STOCHF(data['MAX'], data['MIN'], data['CLOSE'])
 data['RSI'] = talib.RSI(data['CLOSE'], timeperiod=15)
 data['WR'] = talib.WILLR(data['MAX'], data['MIN'], data['CLOSE'], timeperiod=7)
-data['MACD'],signal,hist=talib.MACD(data['CLOSE'])
-data["TRIX"] = talib.TRIX(data['CLOSE'],timeperiod=30)
-data["ROC"] = talib.ROC(data['CLOSE'],timeperiod=10)
-data["MFI"] = talib.MFI(data['MAX'], data['MIN'],data['CLOSE'],data['VOLUME'], timeperiod=14)
-data['ADOSC'] = talib.ADOSC(data['MAX'],data['MIN'], data['CLOSE'], data['VOLUME'], fastperiod=3, slowperiod=10)
-#准备对技术指标进行移位
+data['MACD'], signal, hist = talib.MACD(data['CLOSE'])
+data["TRIX"] = talib.TRIX(data['CLOSE'], timeperiod=30)
+data["ROC"] = talib.ROC(data['CLOSE'], timeperiod=10)
+data["MFI"] = talib.MFI(data['MAX'], data['MIN'], data['CLOSE'], data['VOLUME'], timeperiod=14)
+data['ADOSC'] = talib.ADOSC(data['MAX'], data['MIN'], data['CLOSE'], data['VOLUME'], fastperiod=3, slowperiod=10)
+# 准备对技术指标进行移位
 
 # 数据存储
-data.to_excel('MetaBTCData.xlsx',index=False)
+data.to_excel('MetaBTCData.xlsx', index=False)
--- Neutron Network.py
+++ Neutron Network.py
@@ -3,9 +3,10 @@ from torch import optim
 import torch.nn as nn
 import pandas as pd
 import torch
-import torch.utils.data as td
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import roc_curve
 
 os.chdir('C:/Users/24238/Desktop/毕业设计/毕业论文代码')
 
@@ -14,40 +15,38 @@ os.chdir('C:/Users/24238/Desktop/毕业设计/毕业论文代码')
 class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.Linear1 = nn.Linear(6, 512)  # 第一隐藏层
-        self.Linear2 = nn.Linear(512, 128)
-        self.Linear3 = nn.Linear(128, 1)
+        self.Linear1 = nn.Linear(16, 1024)  # 第一隐藏层
+        self.Linear2 = nn.Linear(1024, 512)
+        self.Linear3 = nn.Linear(512, 1)
 
     def forward(self, x):
         x = torch.sigmoid(self.Linear1(x))
         x = torch.relu(self.Linear2(x))
-        x = self.Linear3(x)
+        x = torch.sigmoid(self.Linear3(x))
         return x
 
 
 if __name__ == '__main__':
     # 数据准备
     data = pd.read_excel('LogisticData.xlsx')
-    fig, ax = plt.subplots(figsize=(3, 2), constrained_layout=True)
 
-    xData = torch.tensor(np.array(data[['CCI', 'MACD', 'RSI', 'WR', 'TRIX', 'VOLUME']]),
+    xData = torch.tensor(np.array(data.drop(['VOLUME', 'RESULT','date'], axis=1)),
                          dtype=torch.float32)
-    yRes = torch.tensor(np.array(data['OPEN']), dtype=torch.float32)
-
-    # test
+    yRes = torch.tensor(np.array(data['RESULT']), dtype=torch.float32)
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(xData, yRes, test_size=0.2, random_state=467898)
 
     # 创建模型
     model = MyModel()
 
     # Loss函数
-    lossFunc = nn.MSELoss()
+    lossFunc = nn.BCELoss()
     # 优化过程
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
-    for epoch in range(10000):
-        yp = model(xData)
+    for epoch in range(800):
+        y_train_p = model(x_train)
         # 计算loss
-        loss = lossFunc(yp.squeeze(-1), yRes)
+        loss = lossFunc(y_train_p.squeeze(-1), y_train)
         # 计算梯度
         loss.backward()
         # 反向传播
@@ -55,29 +54,29 @@ if __name__ == '__main__':
         # 梯度清0
         optimizer.zero_grad()
         if epoch % 100 == 0:
-            print('epoch: {}, loss: {}'.format(epoch, loss.data.item()), 'accuracy:', )
+            y_test_p = model(x_test)
+            threshold = 0.5
+            y_train_p = y_train_p >= threshold
+            y_test_p = y_test_p >= threshold
+            acc_train = float((y_train_p.squeeze(-1) == y_train).sum()) / len(y_train_p)
+            acc_test = float((y_test_p.squeeze(-1) == y_test).sum()) / len(y_test_p)
+            print('epoch: {}, loss: {}'.format(epoch, loss.data.item()), 'accuracy_train:', acc_train, 'accuracy_test:',
+                  acc_test)
+    torch.save(model, 'BP.pt')
     plt.plot(range(525), data['OPEN'])
-    plt.plot(range(525), yp.detach().numpy(), color='red')
+    plt.plot(range(420), y_train_p.detach().numpy(), color='red')
     plt.show()
 
-"""
-    counter = 0
-    ypA=[]
-    for x,y in zip(dataloaderx,dataloadery):
-        # 正向传播
-            yp = model(x)
-            ypA.append(float(yp))
-            # 计算loss
-            loss = lossFunc(yp.squeeze(-1),y)
-            # 计算梯度
-            loss.backward()
-    # 反向传播        
-    optimizer.step()
-    # 梯度清0
-    optimizer.zero_grad()
+
+    def plot_roc_curve(fper, tper):
+        plt.plot(fper, tper, color='red', label='ROC')
+        plt.plot([0, 1], [0, 1], color='green', linestyle='--')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC Curve')
+        plt.legend()
+        plt.show()
 
 
-    if epoch %2 == 0:
-            print(ypA)
-            print('epoch: {}, loss: {}'.format(epoch, loss.data.item()),'accuracy:','acc')
-"""
+    fper, tper, thresholds = roc_curve(y_train, y_train_p.detach().numpy())
+    plot_roc_curve(fper, tper)
